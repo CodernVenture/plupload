3c40cda0ba850ac533bbc65f4e081a492bcbf632
Stoea Leontin <stoea.leontin@gmail.com>
2018-05-01 12:44:57 +0300

    1st test

---
+++ codern/src/SayHello.php
@@ -0,0 +1,9 @@
+<?php
+namespace Codern;
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
---
+++ codern/src/Subfolder/SayHello.php
@@ -0,0 +1,9 @@
+<?php
+namespace Codern\SubFolder;
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, sub Composer!';
+    }
+}
