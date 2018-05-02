a877bee038c466dcb3bde5fc2c24cb00535dcec2
Stoea Leontin <stoea.leontin@gmail.com>
2018-05-02 15:27:04 +0300

    no message

---
+++ big-files-upload/src/BigFilesUpload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stoea_Leontin
+ * Date: 02-May-18
+ * Time: 3:26 PM
+ */
+
+namespace Codern;
+
+
+class BigFilesUpload
+{
+
+}
--- codern/src/SayHello.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Codern;
-class SayHello
-{
-    public static function world()
-    {
-        return 'Hello World, Composer!';
-    }
-}
