d78a652b156e210d62356d45e33a9a6eddff52f3
Stoea Leontin <stoea.leontin@gmail.com>
2018-05-02 16:29:43 +0300

    no message

---
+++ plupload/src/BigFilesUpload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stoea_Leontin
+ * Date: 02-May-18
+ * Time: 4:04 PM
+ */
+
+namespace Codern;
+
+
+class BigFilesUpload
+{
+
+}
--- tests/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
-use Codern\SubFolder\SayHello;
-echo SayHello::world();
